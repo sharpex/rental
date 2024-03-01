dd65e515d3fda9d44f23a728c9530b0622038e44
techsystems <samwelmwangi1995@gmail.com>
2024-03-01 04:05:11 +0300

    'f'

---
+++ static/js/91.b4bbf5ca.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkrentaltechsystem=self.webpackChunkrentaltechsystem||[]).push([[91],{6091:function(e,s,t){t.r(s);t(2791);var n=t(8920),c=t(333),r=t(184);s.default=function(){return(0,r.jsxs)("div",{children:[(0,r.jsx)(n.Z,{}),(0,r.jsx)("div",{style:{paddingTop:"40px"},children:(0,r.jsx)(c.Z,{})})]})}}}]);
+//# sourceMappingURL=91.b4bbf5ca.chunk.js.map
